1d33335be22d928d60882db70857ca4b78a4402b
Robert Brockmann <bbrockmann1@gmail.com>
2022-09-04 12:28:51 -0600

    commit tree 20c2c37ab8cc438f08dd8ab2d9823b9be19bad57

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(array, string){
+    return array.filter(
+        possibleMatch => possibleMatch.toLowerCase() === string.toLowerCase()
+    )
+};
+
+function fuzzyMatch(source, testString) {
+    return source.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+
+function matchName(array, string){
+    return array.filter((record) => record.name === string)
+};
---
+++ scratchpad.js
@@ -0,0 +1,23 @@
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+  function matchName(array, string){
+        console.log(array.filter((array) => array.name === string));
+  };
+
+  matchName(drivers, "Annette");
